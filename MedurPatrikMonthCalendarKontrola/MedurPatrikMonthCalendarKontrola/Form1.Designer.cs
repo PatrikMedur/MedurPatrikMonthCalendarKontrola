544b087815f0bee7bd3fd6e6f44f2b399d3ad05e
PatrikMedur <36442150+PatrikMedur@users.noreply.github.com>
2018-02-27 16:44:45 +0100

    Add files via upload

---
+++ MedurPatrikMonthCalendarKontrola/MedurPatrikMonthCalendarKontrola/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace MedurPatrikMonthCalendarKontrola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.btnTermin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(109, 18);
+            this.monthCalendar1.MaxSelectionCount = 31;
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            // 
+            // btnTermin
+            // 
+            this.btnTermin.Location = new System.Drawing.Point(142, 261);
+            this.btnTermin.Name = "btnTermin";
+            this.btnTermin.Size = new System.Drawing.Size(157, 50);
+            this.btnTermin.TabIndex = 1;
+            this.btnTermin.Text = "Odaberite termin";
+            this.btnTermin.UseVisualStyleBackColor = true;
+            this.btnTermin.Click += new System.EventHandler(this.btnTermin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 358);
+            this.Controls.Add(this.btnTermin);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button btnTermin;
+    }
+}
+
